9c1a9a81c5cde62f9bc1bc3973d6a799113243ac
Damian <c.damiankucharski@gmail.com>
2018-11-30 22:26:46 +0100

    fixed a bug in finding acronyms like USA

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def checkDef(word):
         return data[word]
     elif word.title() in data:
         return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word,data.keys(),3,0.8)) > 0:
         matches = get_close_matches(word,data.keys(),3,0.8)
         yn = input("Did you mean %s instead? [Y/N]: " % matches[0])
