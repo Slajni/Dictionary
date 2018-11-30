9ed54046f5c95c8ddb0666425f57558773af3c40
Damian <c.damiankucharski@gmail.com>
2018-11-30 22:08:15 +0100

    recommending similar word if typo was introduced

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("Resources/data.json"))
 
@@ -6,16 +7,34 @@ def checkDef(word):
     word = word.lower()
     if word in data:
         return(data[word])
+    elif len(get_close_matches(word,data.keys(),3,0.8)) >= 0:
+        matches = get_close_matches(word,data.keys(),3,0.8)
+        yn = input("Did you mean %s instead? [Y/N]: " % matches[0])
+        if yn.lower() == "y":
+            return data[matches[0]]
+        elif yn.lower()=="n":
+            return "There is no such word in database"
+        else:
+            while yn.lower() != "y" and yn.lower() != "n":
+                yn = input("There is no such option, choose [Y/N]: ")
+                if yn.lower() == "y":
+                    return matches[0]
+                elif yn.lower() == "n":
+                    return "There is no such word in database"
+
     else:
-        return("There is no such word in database")
+        return "There is no such word in database"
+
+
 userWord = ""
+
 while userWord.lower()!="N":
     userWord = input("Which word you search for? : ")
     print(checkDef(userWord))
     userWord = input("Do you wish to continue? [Y/N]: ")
-    if(userWord.lower()=="y"):
+    if userWord.lower()=="y":
         continue
-    elif(userWord.lower()=="n"):
+    elif userWord.lower()=="n":
         break
     else:
         while userWord.lower() != "y" and userWord.lower() != "n":
